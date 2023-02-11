717e67cef1efec3af6641fb179ab0162fea616a5
lvyitian <535481388@qq.com>
2023-02-11 17:02:58 +0800

    init

---
+++ SimpleTimer/MainForm.Designer.cs
@@ -0,0 +1,132 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： 53548
+ * 日期: 2023/2/11
+ * 时间: 12:42
+ * 
+ * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
+ */
+namespace DebateTimer
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Timer timer2;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.button1 = new System.Windows.Forms.Button();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.button2 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.timer2 = new System.Windows.Forms.Timer(this.components);
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 272);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(126, 57);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "开始";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// timer1
+			// 
+			this.timer1.Interval = 1;
+			this.timer1.Tick += new System.EventHandler(this.Timer1Tick);
+			// 
+			// button2
+			// 
+			this.button2.Enabled = false;
+			this.button2.Location = new System.Drawing.Point(302, 272);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(126, 57);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "暂停";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.label1.Location = new System.Drawing.Point(12, 43);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(416, 219);
+			this.label1.TabIndex = 3;
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(186, 6);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(221, 28);
+			this.textBox1.TabIndex = 5;
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(12, 9);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(168, 31);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "初始时间(单位:s):";
+			// 
+			// timer2
+			// 
+			this.timer2.Interval = 1;
+			this.timer2.Tick += new System.EventHandler(this.Timer2Tick);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(440, 341);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Name = "MainForm";
+			this.Text = "DebateTimer";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.ControlAdded += new System.Windows.Forms.ControlEventHandler(this.MainFormControlAdded);
+			this.ControlRemoved += new System.Windows.Forms.ControlEventHandler(this.MainFormControlRemoved);
+			this.Resize += new System.EventHandler(this.MainFormResize);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ SimpleTimer/MainForm.cs
@@ -0,0 +1,209 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： 53548
+ * 日期: 2023/2/11
+ * 时间: 12:42
+ * 
+ * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace DebateTimer
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public ulong time;
+		public Stopwatch leftwatch=new Stopwatch();
+		
+		#region 控件缩放
+		public volatile bool isInitialized;
+		public volatile bool isChanging;
+		public volatile Dictionary<string,string> origin;
+        public double formWidth;//窗体原始宽度
+        public double formHeight;//窗体原始高度
+        public double scaleX;//水平缩放比例
+        public double scaleY;//垂直缩放比例
+        //控件中心Left,Top,控件Width,控件Height,控件字体Size
+        /// <summary>
+        /// 获取所有原始数据
+        /// </summary>
+        protected Dictionary<string,string> GetAllInitInfo(Control crlContainer)
+        {
+        	Dictionary<string,string> ret=new Dictionary<string, string>();
+            foreach (Control item in crlContainer.Controls)
+            {
+                if (item.Name.Trim() != "")
+                    ret.Add(item.Name, (item.Left + item.Width / 2) + "," + (item.Top + item.Height / 2) + "," + item.Width + "," + item.Height + "," + item.Font.Size);
+                if ((item as UserControl) == null && item.Controls.Count > 0)
+                    GetAllInitInfo(item);
+            }
+            return ret;
+        }
+        private void ControlsChangeInit(Control crlContainer)
+        {
+            scaleX = (Convert.ToDouble(crlContainer.Width) / formWidth);
+            scaleY = (Convert.ToDouble(crlContainer.Height) / formHeight);
+        }
+        private void ControlsChange(Control crlContainer,Dictionary<string,string> info)
+        {
+            double[] pos = new double[5];//pos数组保存当前控件中心Left,Top,控件Width,控件Height,控件字体Size
+            foreach (Control item in crlContainer.Controls)
+            {
+                if (item.Name.Trim() != "")
+                {
+                    if ((item as UserControl) == null && item.Controls.Count > 0)
+                        ControlsChange(item,info);
+                    string[] strs = info[item.Name].Split(',');
+                    for (int j = 0; j < 5; j++)
+                    {
+                        pos[j] = Convert.ToDouble(strs[j]);
+                    }
+                    double itemWidth = pos[2] * scaleX;
+                    double itemHeight = pos[3] * scaleY;
+                    item.Left = Convert.ToInt32(pos[0] * scaleX - itemWidth / 2);
+                    item.Top = Convert.ToInt32(pos[1] * scaleY - itemHeight / 2);
+                    item.Width = Convert.ToInt32(itemWidth);
+                    item.Height = Convert.ToInt32(itemHeight);
+                    try
+                    {
+                        item.Font = new Font(item.Font.Name, float.Parse((pos[4] * Math.Min(scaleX, scaleY)).ToString()));
+                    }
+                    catch
+                    {
+                    	;
+                    }
+                }
+            }
+        }
+
+        #endregion
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			this.formWidth = Convert.ToDouble(this.Width);
+            this.formHeight = Convert.ToDouble(this.Height);
+            this.isInitialized=true;
+            MainFormLoad(this,null);
+            MainFormResize(this,null);
+            this.timer1.Enabled=true;
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void MainFormResize(object sender, EventArgs e)
+		{
+			if(!this.isInitialized)
+				return;
+			if (!isChanging && this.origin.Count > 0)
+            {
+				this.isChanging=true;
+                ControlsChangeInit(this);
+                ControlsChange(this,this.origin);
+                this.isChanging=false;
+            }
+			if(!isChanging){
+			this.formWidth = Convert.ToDouble(this.Width);
+            this.formHeight = Convert.ToDouble(this.Height);
+			}
+		}
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			this.origin=GetAllInitInfo(this);
+			this.reset();
+			this.StartPosition=FormStartPosition.CenterParent;
+			this.WindowState=FormWindowState.Maximized;
+		}
+		public void reset()
+		{
+			timer2.Enabled=false;
+			resetWatch();
+			textBox1.Enabled=true;
+			label1.Text=millisToText(0);
+			button1.Text="开始";
+			button2.Enabled=false;
+			button2.Text="暂停";
+		}
+		public void resetWatch()
+		{
+			this.leftwatch.Stop();
+			this.leftwatch.Reset();
+		}
+		void MainFormControlAdded(object sender, ControlEventArgs e)
+		{
+			this.origin=GetAllInitInfo(this);
+		}
+		void MainFormControlRemoved(object sender, ControlEventArgs e)
+		{
+			this.origin=GetAllInitInfo(this);
+		}
+		void Timer1Tick(object sender, EventArgs e)
+		{
+			MainFormResize(this,null);
+		}
+		public static string millisToText(long millis)
+		{
+			if(millis>=0) return millisToText((ulong)millis);
+			return "-"+millisToText((ulong)(-millis));
+		}
+		public static string millisToText(ulong millis)
+		{
+			ulong ms=millis%1000;
+			ulong h=millis/1000/60/60;
+			ulong min=millis/1000/60-h*60;
+			ulong s=millis/1000-min*60-h*60*60;
+			return h.ToString("00")+":"+min.ToString("00")+":"+s.ToString("00")+"."+ms.ToString("000");
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			if(button1.Text=="开始")
+			{
+				try{
+					time=ulong.Parse(textBox1.Text.Trim());
+				}catch{
+					MessageBox.Show("开始时间必须是非负整数!");
+					return;
+				}
+				textBox1.Enabled=false;
+				button1.Text="重置";
+				label1.Text=millisToText(time*1000);
+				button2.Enabled=true;
+				timer2.Enabled=true;
+				leftwatch.Start();
+			}else{
+				reset();
+			}
+		}
+		void Timer2Tick(object sender, EventArgs e)
+		{
+			long left=(long)time*1000-leftwatch.ElapsedMilliseconds;
+			label1.Text=millisToText(left);
+			if(left<=0)
+			{
+				reset();
+				MessageBox.Show("时间到!","时间到",MessageBoxButtons.OK,MessageBoxIcon.Information,MessageBoxDefaultButton.Button1);
+				return;
+			}
+		}
+		void Button2Click(object sender, EventArgs e)
+		{
+			if(button2.Text=="暂停")
+			{
+				leftwatch.Stop();
+			    button2.Text="继续";
+			}else{
+				leftwatch.Start();
+			    button2.Text="暂停";
+			}
+		}
+	}
+}
